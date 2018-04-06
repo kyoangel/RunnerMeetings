16ee9b6005d96aa394c5b8da1e321402dfa3197e
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-06 22:19:26 +0800

    Add project files.

---
+++ Kata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RunnerMeetings
+{
+    public class Kata
+    {
+        public int RunnersMeetings(int[] StartPosition, int[] speed)
+        {
+            int result = -1;
+            for (int i = 0; i < StartPosition.Length; i++)
+            {
+                for (int j = i + 1; j < StartPosition.Length; j++)
+                {
+                    MeetingChecker meetingTimeA = MeetingTime(StartPosition[i], StartPosition[j], speed[i], speed[j]);
+                    if (!meetingTimeA.IsMeet)
+                        continue;
+                    int numOfMeet = 2;
+                    for (int k = j + 1; k < StartPosition.Length; k++)
+                    {
+                        MeetingChecker meetingTimeB = MeetingTime(StartPosition[i], StartPosition[k], speed[i], speed[k]);
+                        if (meetingTimeA.Compare(meetingTimeB))
+                            numOfMeet++;
+                    }
+                    result = Math.Max(result, numOfMeet);
+                }
+            }
+            return result;
+        }
+
+        public MeetingChecker MeetingTime(int StartPointA, int StartPointB, int speedA, int speedB)
+        {
+            if (StartPointA == StartPointB)  // meet on startPoint
+                return new MeetingChecker() { Distance = 0, Speed = 0, IsMeet = true };
+            int distance = StartPointA - StartPointB, speed = speedB - speedA;
+
+            if (distance == 0)
+                return new MeetingChecker() { Distance = 0, Speed = 0, IsMeet = true };
+            else if (speed == 0)
+                return new MeetingChecker() { Distance = 0, Speed = 0, IsMeet = false };
+            else if ((distance < 0 && speed > 0) || (distance > 0 && speed < 0))
+                return new MeetingChecker() { Distance = 0, Speed = 0, IsMeet = false };
+            int numOfGCD = GCD(distance, speed);
+            return new MeetingChecker() { Distance = distance / numOfGCD, Speed = speed / numOfGCD, IsMeet = true };
+        }
+
+        public int GCD(int a, int b)
+        {
+            return (a % b == 0) ? b : GCD(b, a % b);
+        }
+    }
+}
---
+++ MeetingChecker.cs
@@ -0,0 +1,33 @@
+﻿namespace RunnerMeetings
+{
+    public class MeetingChecker
+    {
+        public int Distance
+        {
+            set;
+            get;
+        }
+
+        public int Speed
+        {
+            set;
+            get;
+        }
+
+        public bool IsMeet
+        {
+            set;
+            get;
+        }
+
+        public bool Compare(MeetingChecker x)
+        {
+            if ((x.Distance == this.Distance) &&
+                (x.Speed == this.Speed) &&
+                (x.IsMeet == this.IsMeet))
+                return true;
+            else
+                return false;
+        }
+    }
+}
